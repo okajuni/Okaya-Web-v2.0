16382dac19b8528e6a50541d9afab65d7d9b45ef
okajuni <62625411+okajuni@users.noreply.github.com>
2020-12-22 17:51:49 -0500

    Add files via upload

---
+++ js/title.js
@@ -0,0 +1,14 @@
+        var space = "";
+        var speed = "250";
+        var pos = 0;
+        var msg = " Okaya V2.0.1 Bienvenue <|> ";
+        /*
+         c'est un peut long à trouver à faire mais je suis fier et peu de site pense à ça big brain 
+        */
+        function Scroll() {
+            document.title = msg.substring(pos, msg.length) + space + msg.substring(0, pos);
+            pos++;
+            if (pos > msg.length) pos = 0;
+            window.setTimeout("Scroll()", speed);
+        }
+        Scroll(); 
