bb0402504888a20cb174d5edbca1af97d883a0ff
okajuni <62625411+okajuni@users.noreply.github.com>
2021-01-02 21:45:57 +0100

    Update title.js

--- js/title.js
+++ js/title.js
@@ -1,10 +1,8 @@
         var space = "";
+        //plus la valeur de speed est grande plus le texte vas lentement et plus elle est petite plus il vas vite
         var speed = "250";
         var pos = 0;
-        var msg = " Okaya V2.0.1 Bienvenue <|> ";
-        /*
-         c'est un peut long à trouver à faire mais je suis fier et peu de site pense à ça big brain 
-        */
+        var msg = " Okaya V2.0.2 Bienvenue <|> ";
         function Scroll() {
             document.title = msg.substring(pos, msg.length) + space + msg.substring(0, pos);
             pos++;
